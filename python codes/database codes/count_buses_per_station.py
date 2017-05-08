dfb37f7284cb769fb689bbdcf7f7374c16513f8e
aliabdelaal369@gmail.com <mars124563>
2017-05-08 19:28:57 +0200

    updated

---
+++ python codes/database codes/count_buses_per_station.py
@@ -0,0 +1,27 @@
+# this code to count how many buses pass by a station
+
+
+
+import pymysql
+
+db = pymysql.connect("localhost", "root", "ali10395", "Mwaslaty", charset='utf8')
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+sql = "SELECT id FROM Stations"
+cursor.execute(sql)
+stations_id = cursor.fetchall()
+
+for id in stations_id:
+    # find how many time this station was found in the relational table
+    sql = "SELECT station_id FROM Bus_stations WHERE station_id=%d"% id[0]
+    cursor.execute(sql)
+    instances = len(cursor.fetchall())
+    print("station with id",id[0],"appeared ",instances,"times")
+    sql = "UPDATE Stations SET buses_count = %d WHERE ID = %d"%(instances, id[0])
+    cursor.execute(sql)
+
+
+db.commit()
+cursor.close()
+
--- python codes/database codes/stations_database_metros.py
+++ python codes/database codes/stations_database_metros.py
@@ -11,13 +11,13 @@ import csv
 db = pymysql.connect("localhost", "username", "password", "Mwaslaty", charset='utf8')
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
-file = open('stations_location_metro.csv')
+file = open('../../clean data/v1.2/location ready data v1.2/stations_location_metro_updated_v1.2.csv')
 lines = file.readlines()
 
 # create the station if not exit
 
 sql = "CREATE TABLE IF NOT EXISTS Stations(" \
-      "ID INTEGER UNIQUE AUTO_INCREMENT, name VARCHAR(50) UNIQUE NOT NULL , longitude DOUBLE , latitude DOUBLE , metro BIT, bus BIT, PRIMARY KEY (ID)) DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;"
+      "ID INTEGER UNIQUE AUTO_INCREMENT, name VARCHAR(50) UNIQUE NOT NULL , longitude DOUBLE , latitude DOUBLE , metro BIT, bus BIT,centeric BIT,buses_count INTEGER , PRIMARY KEY (ID)) DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;"
 
 try:
     cursor.execute(sql)
@@ -43,8 +43,8 @@ for row in lines:
             print("this couldn't be done because of ",ex)
             pass
         if not found:
-            sql = "INSERT INTO Stations(name,longitude,latitude,metro,bus) VALUE('%s',%f,%f,%d,%d)" % \
-                  (elements[0], float(elements[2]), float(elements[1]), 1, 0)
+            sql = "INSERT INTO Stations(name,longitude,latitude,metro,bus,centeric,buses_count) VALUE('%s',%f,%f,%d,%d,%d,%d)" % \
+                  (elements[0], float(elements[2]), float(elements[1]), 1, 0, 0, 0)
             print(sql)
             try:
                 cursor.execute(sql)
